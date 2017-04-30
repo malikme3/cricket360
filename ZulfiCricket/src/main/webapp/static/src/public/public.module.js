2669ca5a85ddedd0e4a4ee23e72e8c49eb3a2457
zulifqar ahmad <zulifqar.ahmad@hotmail.com>
2017-04-30 08:43:31 -0500

    upgraded angular version to 1.6 and some other libraries

--- ZulfiCricket/src/main/webapp/static/src/public/public.module.js
+++ ZulfiCricket/src/main/webapp/static/src/public/public.module.js
@@ -3,6 +3,6 @@
 /**
  * Public cricket application. Includes the common module and ui-router.
  */
-angular.module('public', ['ui.router', 'common']);
+angular.module('public', ['ui.router', 'common', 'ngMaterial', 'ngMessages']);
 
 })();
